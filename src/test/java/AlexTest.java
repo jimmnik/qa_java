4c17b89235dce11c89cd0733c23914dddeb7e7b0
jimmnik <jimmnik3@gmail.com>
2026-05-18 21:57:28 +0300

    Исправлены замечания: добавлена новая строка в класс Alex, удалены mock и неиспользуемые импорты из AlexTest. Повторно проверено покрытие - изменений нет

--- src/test/java/AlexTest.java
+++ src/test/java/AlexTest.java
@@ -3,15 +3,12 @@ import com.example.Feline;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-@ExtendWith(MockitoExtension.class)
+
 public class AlexTest {
-    @Mock
+
     private Alex alex;
 
     @BeforeEach
